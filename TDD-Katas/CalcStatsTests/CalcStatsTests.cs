ad9bec56b4a5894156af0a30787ed4cb997c345d
sedau79 <sebastian.dau@googlemail.de>
2013-12-07 11:28:13 +0100

    kata

---
+++ TDD-Katas/CalcStatsTests/CalcStatsTests.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using CalcStatsKata;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CalcStatsTests
+{
+    [TestClass]
+    public class CalcStatsTests
+    {
+        private CalcStats _target;
+        private int[] _numbers;
+
+        [TestInitialize]
+        public void SetupTest()
+        {
+            _numbers = new int[] {6, 9, 15, -2, 92, 11};
+            _target = new CalcStats();
+        }
+
+        [TestMethod]
+        public void TestMinimum()
+        {
+            int min = _target.GetMinimum(_numbers);
+            Assert.AreEqual(-2, min);
+        }
+
+        [TestMethod]
+        public void TestMaximum()
+        {
+            int min = _target.GetMaximum(_numbers);
+            Assert.AreEqual(92, min);
+        }
+
+        [TestMethod]
+        public void TestCount()
+        {
+            int min = _target.GetCount(_numbers);
+            Assert.AreEqual(6, min);
+        }
+
+        [TestMethod]
+        public void TestAverage()
+        {
+            double min = _target.GetAverage(_numbers);
+            Assert.AreEqual(21.8333333333333, min,0.00000000001);
+        }
+    }
+}
---
+++ TDD-Katas/StringCalcTests/StringCalcTests.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StringCalc;
+
+namespace StringCalcTests
+{
+    [TestClass]
+    public class StringCalcTests
+    {
+        [TestMethod]
+        [TestCategory("StringCalc")]
+        public void TestEmptyStringSumsUpToZero()
+        {
+            var target = new StringCalculator();
+            Assert.AreEqual(0, target.Add(string.Empty));
+        }
+
+        [TestMethod]
+        [TestCategory("StringCalc")]
+        public void TestNumerInStringSumsUp()
+        {
+            var target = new StringCalculator();
+            Assert.AreEqual(1, target.Add("1"));
+        }
+
+        [TestMethod]
+        [TestCategory("StringCalc")]
+        public void TestNumersInStringSumUp()
+        {
+            var target = new StringCalculator();
+            Assert.AreEqual(66, target.Add("42,24"));
+        }
+
+        [TestMethod]
+        [TestCategory("StringCalc")]
+        public void TestLotsOfNumersInStringSumUp()
+        {
+            var target = new StringCalculator();
+            Assert.AreEqual(131, target.Add("42,24,53,12"));
+        }
+
+        [TestMethod]
+        [TestCategory("StringCalc")]
+        public void TestNumersAndCRLFInStringSumUp()
+        {
+            var target = new StringCalculator();
+            Assert.AreEqual(66, target.Add("42\n,24"));
+        }
+
+        [TestMethod]
+        [TestCategory("StringCalc")]
+        public void TestIfCustomDelimiterWorks()
+        {
+            var target = new StringCalculator();
+            Assert.AreEqual(3, target.Add("//;\n1;2"));
+        }
+    }
+}
